8b3ceca00fc651ee441aebc3abe084768edcb9ac
Tiffany <tiffany.c.stallings@gmail.com>
2018-01-09 12:24:42 -0500

    Set up database table

---
+++ app/models.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Projects(db.Model):
+	__tablename__ = 'projects'
+	id = db.Column(db.String(80), primary_key=True)
+	name = db.Column(db.String(80), nullable=False)
+	date = db.Column(db.DateTime, default=datetime.utcnow)
+	address = db.Column(db.String(200), nullable=False)
+	type = db.Column(db.String(20), nullable=False)
+	status = db.Column(db.String(10) nullable=False)
+	file = db.Column(db.String(80))
+
+	@property
+	def serialize(self):
+		return {
+			'id': self.id,
+			'name': self.name,
+			'date': self.date,
+			'address': self.address,
+			'type': self.type,
+			'status': self.status,
+			'file': self.file
+		}
